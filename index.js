e514e9f7a39f0946708dd1e78a9a012cf26823b7
wi1dcode <magosdk@icloud.com>
2022-07-19 14:25:25 +0200

    Exc 3 done? for fix after

--- index.js
+++ index.js
@@ -35,12 +35,37 @@ const checkTemperature = () => {
 };
 
 
-const TempResult = async () => {
+const tempResult = async () => {
     const result = await checkTemperature()
     console.log(result)
   }
   
-TempResult()
+tempResult()
 
 
 // Ex 3
+
+
+
+const menageStatus = () => {
+    return new Promise((resolve, reject) => {
+        console.log("Je commence la lessive")
+        setTimeout(() => {
+            console.log("j’ai fini la lessive");
+        }, 3000);
+        console.log("Je commence  la vaisselle")
+        setTimeout(() => {
+            console.log("j’ai fini la vaisselle");
+        }, 1500);
+
+    })
+}
+
+
+const menageResult = async () => {
+    const result = await menageStatus()
+    console.log(result)
+    console.log("J’ai fini de faire le ménage");
+  }
+  
+menageResult()
